357b91f4a400a8d32048020e7de6ab48f32bf9f9
Automation3026 <automationengineer3026@gmail.com>
2022-07-07 16:43:49 +0530

    Initial Commit

---
+++ GitDemo/src/org/github/GitDemoClass.java
@@ -0,0 +1,12 @@
+package org.github;
+
+public class GitDemoClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("Hi");
+		System.out.println("how r u");
+	}
+
+}
