71086042b8a916c6d1ff52679cb5d2a5769df0db
Automation3026 <108859343+Automation3026@users.noreply.github.com>
2022-08-02 17:53:50 +0530

    Update GitDemoClass.java

--- GitDemo/src/org/github/GitDemoClass.java
+++ GitDemo/src/org/github/GitDemoClass.java
@@ -7,9 +7,9 @@ public class GitDemoClass {
 		System.out.println("Hello World");
 		System.out.println("Hi");
 		System.out.println("how r u");
-		System.out.println("Hi Aravind);
+		System.out.println("Hi Aravind); Need more new functionalities
 				   
-		System.out.println("How r u);
+		System.out.println("How r u"); Addiing login funtionality
 			System.out.println("I m doing good);	   
 	}
 
